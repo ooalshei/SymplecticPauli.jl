e912a5dc67a0530a26248eddd900d25446eca992
ooalshei <ooalshei@ncsu.edu>
2025-07-01 08:59:04 -0700

    fixed imports

--- src/SymplecticPauli.jl
+++ src/SymplecticPauli.jl
@@ -1,14 +1,14 @@
 module SymplecticPauli
 
-export I, σx, σy, σz, ⊗, AbstractPauli, UPauli, Pauli, PauliList, PauliSentence, toint, tostring, com, ad, ad!, countx, county, countz, tomatrix
+export I, σx, σy, σz, σy_real, ⊗, AbstractPauli, UPauli, Pauli, PauliList, PauliSentence, toint, tostring, com, ad, ad!, countx, county, countz, counti, tomatrix
 
-using LinearAlgebra: I
+using LinearAlgebra: I, norm, tr
 
 const C8 = Complex{Int8}
-const σx = [0 1; 1 0]
-const σy = [0 -im; im 0]
-const σz = [1 0; 0 -1]
-const σy_real = -im * σy
+const σx = Int8[0 1; 1 0]
+const σy = C8[0 -im; im 0]
+const σz = Int8[1 0; 0 -1]
+const σy_real = Int8[0 -1; 1 0]
 const ⊗ = kron
 
 include("pauli.jl")
